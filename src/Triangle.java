a18914074e72289d60a8c4a4ca70ce45c739924f
OtavioRMachado <otavio@inf.ufsm.br>
2015-02-19 18:39:04 -0200

    Adding vertical lines function. I refactored the code so both horizontal lines and vertical lines can be constructed by the same function, since they are mainly the same thing.

--- src/Triangle.java
+++ src/Triangle.java
@@ -7,4 +7,23 @@ public class Triangle {
     public String Print() {
         return "*";
     }
+
+    public String CreateLine(int elements_in_line, boolean isVertical) {
+        String result = "";
+        int last_element = elements_in_line - 1; /* This variable should not be created if it is a horizontal line. What should I do? */
+        for(int i = 0; i < elements_in_line; i++) {
+           result += "*";
+            if(isVertical && i != last_element) {
+                result += "\n";
+            }
+        }
+        return result;
+    }
+    public String PrintHLine(int elements_in_line) {
+        return CreateLine(elements_in_line, false);
+    }
+
+    public String PrintVLine(int elements_in_line) {
+        return CreateLine(elements_in_line, true);
+    }
 }
--- src/TriangleTest.java
+++ src/TriangleTest.java
@@ -1,12 +1,33 @@
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TriangleTest {
+    private Triangle t;
+    @Before
+    public void setup() {
+        t = new Triangle();
+    }
+
     @Test
     public void basic_triangle_should_return_one_asterisk() {
-        Triangle t = new Triangle();
         assertEquals("*", t.Print());
     }
 
+    @Test
+    public void should_return_empty_line() {
+        assertEquals("", t.PrintHLine(0));
+    }
+
+    @Test
+    public void should_draw_one_line_with_five_asterisks() {
+        assertEquals("*****", t.PrintHLine(5));
+    }
+
+    @Test
+    public void should_draw_one_vertical_line_with_two_asterisks() {
+        assertEquals("*\n*", t.PrintVLine(2));
+    }
 }
